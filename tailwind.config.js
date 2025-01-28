385712bb7011a7db4741a7aaabfe3c14bf5d96ed
Louis Fernando <ferlouis55@gmail.com>
2025-01-28 17:41:45 +0700

    Set up TailwindCSS, other dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        dark: '#252525',
+        light: '#fafafa',
+        primary: '#ffc300',
+        darkNavy: '#222d3e',
+        lightNavy: '#404558',
+        darkOrange: '#e0642e',
+        lightOrange: '#f8a557',
+        cream: '#fce7cc'
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        dynaPuff: ["DynaPuff", "sans-serif"],
+        sans: ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+  ],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
