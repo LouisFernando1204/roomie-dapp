bf78b0338ad78881d11a1378348a760d3f3ff1de
yebology <139052533+yebology@users.noreply.github.com>
2025-01-30 14:04:56 +0700

    setup reown (walletconnect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { AppKitNetwork, holesky } from "@reown/appkit/networks";
+
+const projectId = import.meta.env.VITE_PROJECT_ID;
+
+const networks: [AppKitNetwork] = [holesky]
+
+const metadata = {
+  name: "Roomie", 
+  description: "Web3 proj",
+  url: "http://127.0.0.1:5173",
+  icons: ["https://avatars.roomie.com/"],
+}
+
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks,
+  metadata,
+  projectId
+})
+
 function App() {
   return (
     <>
--- src/services/connector.ts
+++ src/services/connector.ts
@@ -2,17 +2,21 @@ import { BrowserProvider, Contract, JsonRpcProvider } from "ethers";
 import contractAbi from "../abi/contractAbi.json";
 
 const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-const networkRPC = import.meta.env.NETWORK_RPC; 
+const networkRPC = import.meta.env.VITE_NETWORK_RPC;
+
+// change to :
+// const { walletProvider } = useAppKitProvider('eip155')
+// const provider = new ethers.BrowserProvider((walletProvider) as any);
 
 export async function getContractWithSigner() {
-    const provider = new BrowserProvider((window as any).ethereum);
-    const signer = await provider.getSigner();
-    const contract = new Contract(contractAddress, contractAbi, signer);
-    return contract;
+  const provider = new BrowserProvider((window as any).ethereum);
+  const signer = await provider.getSigner();
+  const contract = new Contract(contractAddress, contractAbi, signer);
+  return contract;
 }
 
 export async function getContractWithoutSigner() {
-    const provider = new JsonRpcProvider(networkRPC);
-    const contract = new Contract(contractAddress, contractAbi, provider);
-    return contract;
+  const provider = new JsonRpcProvider(networkRPC);
+  const contract = new Contract(contractAddress, contractAbi, provider);
+  return contract;
 }
